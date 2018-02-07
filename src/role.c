be9eb18f7ac10b983124749d3f6d70cc9b97684d
AmaurX <amaury.camus92@gmail.com>
2018-02-07 16:06:21 +0100

    added masterloop

--- src/role.c
+++ src/role.c
@@ -14,13 +14,74 @@ enum role giveRoleInGroup(int rankGroup)
         return slave;
 }
 
-void masterLoop(MPI_Comm *groupCommList, animated_gif *image)
+void masterLoop(int *groupMasterList, int numberOfGroupMaster, animated_gif *image)
 {
     int count = 0;
+    int numberOfImages = image->n_images;
 
-    while (count < image->n_images)
+    // int doneImages[numberOfImages] = {0};
+    int numberOfProcessedImages = 0;
+    for (int i = 0; i < numberOfGroupMaster; i++)
     {
-        count++;
+        if (count < numberOfImages)
+        {
+            struct task newTask;
+
+            newTask.id = count;
+            newTask.frameNumber = count;
+            newTask.width = image->width[count];
+            newTask.height = image->height[count];
+
+            MPI_Send((void *)&newTask, 1, MPI_CUSTOM_TASK,
+                     groupMasterList[i], TASK_TAG, MPI_COMM_WORLD);
+
+            int numberOfPixels = newTask.height * newTask.width;
+            printf("Sending frame %d on %d", count, numberOfImages);
+            MPI_Send((void *)(image + count), numberOfPixels, MPI_CUSTOM_PIXEL,
+                     (int)groupMasterList[i], IMAGE_TAG, MPI_COMM_WORLD);
+            count++;
+        }
+    }
+
+    while (numberOfProcessedImages < numberOfImages)
+    {
+        struct task doneTask;
+        MPI_Status status;
+
+        MPI_Recv((void *)&doneTask, 1, MPI_CUSTOM_TASK, MPI_ANY_SOURCE,
+                 TASK_TAG, MPI_COMM_WORLD, &status);
+
+        int frameNumber = doneTask.frameNumber;
+        int numberOfPixels = doneTask.width * doneTask.height;
+        int sender = status.MPI_SOURCE;
+        MPI_Recv((void *)(image + frameNumber), numberOfPixels, MPI_CUSTOM_PIXEL, sender,
+                 IMAGE_TAG, MPI_COMM_WORLD, &status);
+
+        // doneImages[doneTask.frameNumber] = 1;
+        if (status.MPI_ERROR == 0)
+        {
+            numberOfProcessedImages++;
+            printf("Frame %d on %d was processed successfully. %d frame completed in total", frameNumber, numberOfImages, numberOfProcessedImages);
+        }
+
+        if (count < numberOfImages)
+        {
+            struct task newTask;
+
+            newTask.id = count;
+            newTask.frameNumber = count;
+            newTask.width = image->width[count];
+            newTask.height = image->height[count];
+
+            MPI_Send((void *)&newTask, 1, MPI_CUSTOM_TASK,
+                     sender, TASK_TAG, MPI_COMM_WORLD);
+
+            int numberOfPixels = newTask.height * newTask.width;
+
+            MPI_Send((void *)(image + count), numberOfPixels, MPI_CUSTOM_PIXEL,
+                     sender, IMAGE_TAG, MPI_COMM_WORLD);
+            count++;
+        }
     }
 }
 
